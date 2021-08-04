ef241fa8f1bbc4da8eb7cb07b5884dd414e4c0a0
Roman <roma00062@gmail.com>
2021-08-04 18:19:05 +0200

    Added fucnction TimeRemaning

--- js/script.js
+++ js/script.js
@@ -1,41 +1,106 @@
 "use strict";
 document.addEventListener('DOMContentLoaded', () => {
+
+    ///////////////////////////////////////////////////////////
+    /////////////////   Tabs   //////////////////////////////
+    ////////////////////////////////////////////////////////
     const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
+        tabsContent = document.querySelectorAll('.tabcontent'),
+        tabsParent = document.querySelector('.tabheader__items');
 
-          function hideTabContent() {
-              tabsContent.forEach(item => {
-                    item.classList.add('hide');
-                    item.classList.remove('show', 'fade');
-              });
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
 
-              tabs.forEach(item => {
-                    item.classList.remove('tabheader__item_active');
-              });
-          }
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
 
 
-          function showTabContent(i = 0) {
-              tabsContent[i].classList.add('show', 'fade');
-              tabsContent[i].classList.remove('hide');
-              tabs[i].classList.add('tabheader__item_active');
-          }
+    function showTabContent(i = 0) {
+        tabsContent[i].classList.add('show', 'fade');
+        tabsContent[i].classList.remove('hide');
+        tabs[i].classList.add('tabheader__item_active');
+    }
 
-          hideTabContent();
-          showTabContent();
+    hideTabContent();
+    showTabContent();
 
-          tabsParent.addEventListener('click', (e) => {
-            const target = e.target;
+    tabsParent.addEventListener('click', (e) => {
+        const target = e.target;
 
-            if (target && target.classList.contains('tabheader__item')) {
-                tabs.forEach((item, i) => {
-                    if (target == item) {
-                        hideTabContent();
-                        showTabContent(i);              
-                    }
-                });
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i) => {
+                if (target == item) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+    ///////////////////////////////////////////////////////////
+    /////////////////   Timer   //////////////////////////////
+    ////////////////////////////////////////////////////////
+
+    const deadline = '2021-08-21';
+
+    function getTimeRemaning(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),//Date.parse Строковое представление даты 
+            days = Math.floor(t / (1000 * 60 * 60 * 24)), //Сщитаем количество дней.  Math.floor(округляет числа)
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+
+
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();//вызовем  функцию здесь чтоб избежать мигания на странице
+
+        function updateClock() {
+            const t = getTimeRemaning(endtime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
             }
-          });
 
+
+        }
+    }
+
+    setClock('.timer', deadline);
+
+    
 });
